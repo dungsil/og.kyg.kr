9f263f700d4c56755c97a5d391466caac21f267f
김용건(Kim Younggeon) <mail@kyg.kr>
2021-08-02 13:14:39 +0900

    fix: 붙여넣기 실수

--- api/libs/make-html.ts
+++ api/libs/make-html.ts
@@ -27,7 +27,7 @@ function getTemplate (options: RequestOptions): string {
     .toString('utf-8')
     .replace(/{{\s+?pattern\s+?}}/g, escapeHTML(options.pattern ?? ''))
     .replace(/{{\s+?background\s+?}}/g, options.background ?? '#ffffff')
-    .replace(/{{\s+?color\s+?}}/g, options.background ?? '#343a40')
+    .replace(/{{\s+?color\s+?}}/g, options.color ?? '#343a40')
     .replace(/{{\s+?text\s+?}}/g, escapeHTML(options.text))
     .replace(/{{\s+?style\s+?}}/g, getCSSTemplate())
     .replace(/{{\s+?icon\s+?}}/g, icons.join('<span class="icon plus">+</span>'))
